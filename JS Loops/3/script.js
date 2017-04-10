a947bc25e6f5137670b325c08962e1df30151218
kacperpll <kacper.duniewicz@gmail.com>
2017-04-10 16:09:14 +0200

    init

---
+++ JS Loops/3/script.js
@@ -0,0 +1,32 @@
+console.log("ZADANIE1");
+for (var i = -10; i < 20; i++)
+{
+  console.log(i);
+}
+
+console.log("ZADANIE 2");
+for (var i = 10; i < 41; i++)
+{
+  if (i%2 === 0)
+  {
+    console.log(i);
+  }
+}
+
+console.log("ZADANIE 3");
+for (var i = 300; i < 334; i++)
+{
+  if (i%2 !== 0)
+  {
+    console.log(i);
+  }
+}
+
+console.log("ZADANIE 4");
+for (var i = 5; i < 51; i++)
+{
+  if (i%5 === 0 && i%3 === 0)
+  {
+    console.log(i);
+  }
+}
