5863211891bc2a976c029eb7d92e9df571a73933
kacperpll <kacper.duniewicz@gmail.com>
2017-05-08 19:54:05 +0200

    init

--- DOM/Score_Keeper/script.js
+++ DOM/Score_Keeper/script.js
@@ -1,17 +1,30 @@
+var pOne = document.querySelector("#cos1");
+var pTwo = document.querySelector("#cos2");
 var scoreOne = 0;
 var scoreTwo = 0;
 var game = 5;
 var wynik = 0;
 
-function addPlayerOne() {
+pOne.addEventListener("click", function(){
   if (game>scoreOne&&game>scoreTwo) {
-    scoreOne += 1;
+    scoreOne++;
     document.querySelector("#first").textContent = scoreOne;
     if (scoreOne == game) {
     document.querySelector("#first").style.color = "green";
     }
   }
-}
+})
+
+// function addPlayerOne() {
+//   if (game>scoreOne&&game>scoreTwo) {
+//     scoreOne += 1;
+//     document.querySelector("#first").textContent = scoreOne;
+//     if (scoreOne == game) {
+//     document.querySelector("#first").style.color = "green";
+//     }
+//   }
+// }
+
 function addPlayerTwo() {
   if (game>scoreOne&&game>scoreTwo) {
     scoreTwo += 1;
@@ -24,7 +37,7 @@ function addPlayerTwo() {
 function reset(){
   scoreOne = scoreTwo = 0;
   // scoreTwo = 0;
-  
+
   document.querySelector("#first").textContent = scoreOne;
   document.querySelector("#second").textContent = scoreTwo;
   document.querySelector("#in").value = null;
